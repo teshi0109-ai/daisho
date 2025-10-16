c630f9dce1cd160bb0fdc399c50c7aeea01f2427
haruhapi <te_shi0109@icloud.com>
2025-10-16 15:23:03 +0900

    20251016 daisho-dai1

---
+++ daisho-day1/script.js
@@ -0,0 +1,13 @@
+const helloBtn=document.getElementById("helloBtn");
+const massage=document.getElementById("massage");
+
+helloBtn.addEventListener("click",()=>{
+    const hour =new Date().getHours();
+    let greeting="こんにちは！";
+    if(hour<12){
+        greeting="おはようございます！";
+    }else if (hour>=18){
+        greeting="こんばんは！";
+    }
+    message.textContent='${greeting}GitHub Pages の公開に成功しました！';
+});
