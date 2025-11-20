cf42b8810af82b8e04d21fb46d2355499177a79d
haruhapi <te_shi0109@icloud.com>
2025-11-20 15:00:17 +0900

    1

---
+++ daisho-day3/assets/js/inventory.js
@@ -0,0 +1,60 @@
+//-----------------
+//1.在庫データ(JSON的な構造)
+//-----------------
+const items=[
+    {id:"D001",name:"オレンジジュース",price:150,stock:20,minStock:5},
+    {id:"D002",name:"コーラ",         price:150,stock:12,minStock:5},
+    {id:"D003",name:"お茶",           price:120,stock:30,minStock:10}
+];
+
+//HTMLで <tbody id="item-list"> と書いた部分を JavaScript から取得する
+const tbody=document.getElementsByld("item-list")
+
+//-----------------
+//2.表示用の関数render()
+//-----------------
+function render(){
+    tbody.innerHTML="",
+
+
+    items.forEach(item=>{
+        const tr=document.createElement("tr");
+
+
+        if(item.stock<item.minStock){
+            tr.classList.add("low-stoock");
+        }
+
+
+        tr.innerHTML=`
+        <td>${item.id}</td>
+        <td>${item.name}</td>
+        <td>${item.price}</td>
+        <td>${item.stock}</td>
+        <td>
+           <button onclick="chargeStock('${item.id}',1)">+</button>
+           <button onclick="chargeStock('${item.id}',-1)">-</button>
+        </td>
+        `;
+
+        tbody.appendChild(tr);
+    });
+}
+
+//-----------------
+function chargeStock(id,diff){
+    const item=items.find(i=>i.id===id);
+    if(!item){
+        return;
+    }
+
+    item.stock+=diff
+
+    if(item.stock<0){
+        item.stock=0;
+    }
+
+    render();
+}
+
+render();
