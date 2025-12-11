1bf3f189cc9d850a9ded03a587236e4ff5dc82c1
haruhapi <te_shi0109@icloud.com>
2025-12-11 14:31:18 +0900

    [

--- daisho-day4/assets/css/js/inventory.js
+++ daisho-day4/assets/css/js/inventory.js
@@ -1,8 +1,8 @@
 // ▼ 在庫データ（デフォルト値：localStorageに何もないとき用）
  let items = [
   { id: "D001", name: "オレンジジュース", price: 150, stock: 20, minStock: 5 },
-  { id: "D002", name: "コーラ",           price: 150, stock: 12, minStock: 5 },
-  { id: "D003", name: "お茶",             price: 120, stock: 30, minStock: 10 }
+  { id: "D002", name: "コーラ",          price: 150, stock: 12, minStock: 5 },
+  { id: "D003", name: "お茶",            price: 120, stock: 30, minStock: 10 }
 ];
 
 // ▼ localStorage に保存する際のキー名（引き出しのラベル）
